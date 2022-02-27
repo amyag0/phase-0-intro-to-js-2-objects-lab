c7b76e273630c203370f122c961d79195ad55231
amyag0 <amyago19@gmail.com>
2022-02-27 14:58:36 -0500

    commit tree 1231c72e140ea9786b517568d9b34be59ac7b1c7

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway",
+  }; // define employee variable
+  
+function updateEmployeeWithKeyAndValue( employee , key, value) {
+    return Object.assign({}, employee, { [key]: value });
+} //function that takes 3 arguments; doesnt  mutate employee
+
+function destructivelyUpdateEmployeeWithKeyAndValue(driver, key, value) {
+    employee[key] = value;
+    return employee;
+} //same deal but destructive
+  
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = Object.assign({}, employee);//create new employee that is given values of employee
+    delete newEmployee[key];
+    return newEmployee;
+}
+  
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
